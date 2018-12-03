9a1dcb9f40de566281b08c3d1e5ea595a4bd9c0a
Alton Salmon <alton.salmon34@gmail.com>
2018-12-02 21:45:22 -0500

    added feature for coin buttons

--- public/js/index.js
+++ public/js/index.js
@@ -1,44 +1,52 @@
 "use strict";
 
 (function() {
-  function getSevenDayCoinPerformance(coin) {
-    $.get("/api/" + coin, function(data) {
-      var chartData = [];
-      for (var close in data) {
-        if (data.hasOwnProperty(close)) {
-          chartData.push(data[close].close);
-        }
-      }
-      renderLineChart(chartData);
-    });
-  }
-
-  function renderLineChart(chartData) {
-    var sdpChartElem = document
-      .getElementById("seven-day-performance")
-      .getContext("2d");
-    new Chart(sdpChartElem, {
-      type: "line",
-      data: {
-        labels: [
-          "Sunday",
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Friday",
-          "Saturday"
-        ],
-        datasets: [
-          {
-            label: "Seven Day Performance",
-            data: chartData,
-            borderColor: "#3cba9f"
-            //fill: false
-          }
-        ]
-      }
-    });
-  }
-
+  $(document).on("click", ".coin-button", coinButtonHandler);
   getSevenDayCoinPerformance("BTH");
 })();
+
+function coinButtonHandler() {
+  var coin = $(this).data("button");
+  console.log($(this));
+  $("#seven-day-performance").empty();
+  getSevenDayCoinPerformance(coin);
+}
+
+function getSevenDayCoinPerformance(coin) {
+  $.get("/api/" + coin, function(data) {
+    var chartData = [];
+    for (var close in data) {
+      if (data.hasOwnProperty(close)) {
+        chartData.push(data[close].close);
+      }
+    }
+    renderLineChart(chartData);
+  });
+}
+
+function renderLineChart(chartData) {
+  var sdpChartElem = document
+    .getElementById("seven-day-performance")
+    .getContext("2d");
+  new Chart(sdpChartElem, {
+    type: "line",
+    data: {
+      labels: [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Friday",
+        "Saturday"
+      ],
+      datasets: [
+        {
+          label: "Seven Day Performance",
+          data: chartData,
+          borderColor: "#3cba9f"
+          //fill: false
+        }
+      ]
+    }
+  });
+}
