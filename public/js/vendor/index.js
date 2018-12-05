6b00fdbc950c3076c808669508a210e01fdadca0
Grover <41809783+vwhope@users.noreply.github.com>
2018-12-04 23:20:36 -0500

    commit vendor file change

---
+++ public/js/vendor/index.js
@@ -0,0 +1,50 @@
+"use strict";
+
+(function() {
+  $(document).on("click", ".coin-button", coinButtonHandler);
+  getSevenDayCoinPerformance("BTH");
+})();
+
+function coinButtonHandler() {
+  var coin = $(this).data("button");
+  $("#seven-day-performance").empty();
+  getSevenDayCoinPerformance(coin);
+}
+
+function getSevenDayCoinPerformance(coin) {
+  $.get("/api/" + coin, function(data) {
+    var ChartData = {
+      closingPoints: [],
+      day: []
+    };
+    for (var close in data) {
+      if (data.hasOwnProperty(close)) {
+        ChartData.closingPoints.push(data[close].close);
+        console.log(data[close].close);
+        ChartData.day.push(moment(data[close].time * 1000).format("dddd"));
+        console.log(moment(data[close].time * 1000).format("dddd"));
+      }
+    }
+    renderLineChart(ChartData);
+  });
+}
+
+function renderLineChart(chartData) {
+  var sdpChartElem = document
+    .getElementById("seven-day-performance")
+    .getContext("2d");
+  new Chart(sdpChartElem, {
+    type: "line",
+    data: {
+      labels: chartData.day,
+      datasets: [
+        {
+          label: "Seven Day Performance",
+          data: chartData.closingPoints,
+          borderColor: "#3cba9f",
+          fill: false
+        }
+      ]
+    }
+  });
+}
