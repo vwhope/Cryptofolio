692354d4ab1dbc3069ed8925fcb9664c46fa1996
Alton Salmon <alton.salmon34@gmail.com>
2018-12-09 17:18:12 -0500

    refactored code and group functions that are used throughout the whole program to utils.js

--- public/js/index.js
+++ public/js/index.js
@@ -14,24 +14,6 @@ $(document).ready(function() {
   });
 });
 
-function isLoggedIn(callback) {
-  console.log("isLogged got called");
-  $.ajax({
-    type: "GET",
-    url: "/api/isLoggedIn/",
-    success: function(data) {
-      console.log("user is logged in");
-      console.log(data.email);
-      displayUserDropDown(data.email);
-      callback(true, data.email);
-    },
-    error: function() {
-      console.log("user is not logged in");
-      callback(false);
-    }
-  });
-}
-
 function loginHandler(event) {
   // Prevent page from reloading
   event.preventDefault();
@@ -65,10 +47,6 @@ function loginHandler(event) {
   }
 }
 
-function displayUserDropDown(email) {
-  $("#email").text(email);
-}
-
 function displayInfoAfterLogin(user) {
   $("#myModal").modal("hide");
   $("#main-content-area").css("visibility", "visible");
@@ -82,78 +60,6 @@ function getSnapshot(user) {
     renderRecentTransactions(data.Transactions);
     renderThreeOrLessCoins(data.Portfolios);
     renderAccountTotal(data.Portfolios);
-  });
-}
-
-function renderThreeOrLessCoins(portfolioData) {
-  var numberOfCoinsToDisplay = 3;
-  var iterator = 0;
-  var newUl = $("<ul>").addClass("list-group list-group-flush coin-snapshot");
-
-  if (portfolioData.length > numberOfCoinsToDisplay) {
-    while (iterator < numberOfCoinsToDisplay) {
-      newUl.append(
-        $("<li>")
-          .addClass("list-group-item")
-          .text(
-            portfolioData[iterator].coin +
-              " " +
-              portfolioData[iterator].holdings
-          )
-      );
-      iterator += 1;
-    }
-    $("#partial-coin-snapshot").append(newUl);
-  } else {
-    for (var coin in portfolioData) {
-      if (portfolioData.hasOwnProperty(coin)) {
-        newUl.append(
-          $("<li>")
-            .addClass("list-group-item")
-            .text(portfolioData[coin].coin + " " + portfolioData[coin].holdings)
-        );
-      }
-    }
-  }
-  $("#partial-coin-snapshot").append(newUl);
-}
-
-function renderHoldingsPiChart(portfolioData) {
-  var chartData = {
-    holdings: [],
-    coin: [],
-    color: [],
-    highlights: []
-  };
-
-  var numberOfCoins = portfolioData.length;
-  var i;
-  for (i = 0; i < numberOfCoins; i++) {
-    var r = Math.floor(Math.random() * 200);
-    var g = Math.floor(Math.random() * 200);
-    var b = Math.floor(Math.random() * 200);
-    var c = "rgb(" + r + ", " + g + ", " + b + ")";
-    var h = "rgb(" + (r + 20) + ", " + (g + 20) + ", " + (b + 20) + ")";
-    chartData.holdings.push(portfolioData[i].holdings);
-    chartData.coin.push(portfolioData[i].coin);
-    chartData.color.push(c);
-    chartData.highlights.push(h);
-  }
-
-  var sdpChartElem = document
-    .getElementById("holdings-doughnut-chart")
-    .getContext("2d");
-  new Chart(sdpChartElem, {
-    type: "pie",
-    data: {
-      labels: chartData.coin,
-      datasets: [
-        {
-          data: chartData.holdings,
-          backgroundColor: chartData.color
-        }
-      ]
-    }
   });
 }
 
@@ -232,19 +138,4 @@ function renderLineChart(chartData) {
       ]
     }
   });
-}
-
-function renderAccountTotal(portfolio) {
-  var total = 0;
-  for (var i = 0; i < portfolio.length; i++) {
-    total += parseFloat(portfolio[i].holdings) * portfolio[i].price;
-  }
-  var totalTitle = $("<div class='total-title'>")
-    .addClass("account-total")
-    .text("Account Balance:");
-  var totalAmount = $("<div class='total-balance'>")
-    .addClass("account-total")
-    .text("$" + total.toFixed(2));
-  $("#portfolio-total").append(totalTitle);
-  $("#portfolio-total").append(totalAmount);
 }
---
+++ public/js/utils.js
@@ -0,0 +1,105 @@
+"use strict";
+function isLoggedIn(callback) {
+  $.ajax({
+    type: "GET",
+    url: "/api/isLoggedIn/",
+    success: function(data) {
+      displayUserDropDown(data.email);
+      callback(true, data.email);
+    },
+    error: function() {
+      callback(false);
+    }
+  });
+}
+
+function displayUserDropDown(email) {
+  $("#email").text(email);
+}
+
+function renderHoldingsPiChart(portfolioData) {
+  var chartData = {
+    holdings: [],
+    coin: [],
+    color: [],
+    highlights: []
+  };
+
+  var numberOfCoins = portfolioData.length;
+  var i;
+  for (i = 0; i < numberOfCoins; i++) {
+    var r = Math.floor(Math.random() * 200);
+    var g = Math.floor(Math.random() * 200);
+    var b = Math.floor(Math.random() * 200);
+    var c = "rgb(" + r + ", " + g + ", " + b + ")";
+    var h = "rgb(" + (r + 20) + ", " + (g + 20) + ", " + (b + 20) + ")";
+    chartData.holdings.push(portfolioData[i].holdings);
+    chartData.coin.push(portfolioData[i].coin);
+    chartData.color.push(c);
+    chartData.highlights.push(h);
+  }
+
+  var sdpChartElem = document
+    .getElementById("holdings-doughnut-chart")
+    .getContext("2d");
+  new Chart(sdpChartElem, {
+    type: "pie",
+    data: {
+      labels: chartData.coin,
+      datasets: [
+        {
+          data: chartData.holdings,
+          backgroundColor: chartData.color
+        }
+      ]
+    }
+  });
+}
+
+function renderAccountTotal(portfolio) {
+  var total = 0;
+  for (var i = 0; i < portfolio.length; i++) {
+    total += parseFloat(portfolio[i].holdings) * portfolio[i].price;
+  }
+  var totalTitle = $("<div class='total-title'>")
+    .addClass("account-total")
+    .text("Account Balance:");
+  var totalAmount = $("<div class='total-balance'>")
+    .addClass("account-total")
+    .text("$" + total.toFixed(2));
+  $("#portfolio-total").append(totalTitle);
+  $("#portfolio-total").append(totalAmount);
+}
+
+function renderThreeOrLessCoins(portfolioData) {
+  var numberOfCoinsToDisplay = 3;
+  var iterator = 0;
+  var newUl = $("<ul>").addClass("list-group list-group-flush coin-snapshot");
+
+  if (portfolioData.length > numberOfCoinsToDisplay) {
+    while (iterator < numberOfCoinsToDisplay) {
+      newUl.append(
+        $("<li>")
+          .addClass("list-group-item")
+          .text(
+            portfolioData[iterator].coin +
+              " " +
+              portfolioData[iterator].holdings
+          )
+      );
+      iterator += 1;
+    }
+    $("#partial-coin-snapshot").append(newUl);
+  } else {
+    for (var coin in portfolioData) {
+      if (portfolioData.hasOwnProperty(coin)) {
+        newUl.append(
+          $("<li>")
+            .addClass("list-group-item")
+            .text(portfolioData[coin].coin + " " + portfolioData[coin].holdings)
+        );
+      }
+    }
+  }
+  $("#partial-coin-snapshot").append(newUl);
+}
