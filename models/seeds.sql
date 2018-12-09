f3b574da41d31d816ec0d5b5c2687685ea8b3102
Alton Salmon <alton.salmon34@gmail.com>
2018-12-09 01:30:17 -0500

    resolved seed conflict

--- models/seeds.sql
+++ models/seeds.sql
@@ -4,12 +4,12 @@ INSERT INTO Users (firstName, lastName, email, password, createdAt, updatedAt)
 VALUES ("demoFirst", "demoLast", "demo@cryptofolio.com", "myword",  "2018-12-03 04:01:00",  "2018-12-03 04:01:00"),
 ("demoFirst2", "demoLast2", "demo2@cryptofolio.com", "myword2",  "2018-12-03 04:01:00",  "2018-12-03 04:01:00");
 
-INSERT INTO Portfolios (coin, holdings, own, createdAt, updatedAt, UserEmail)
+INSERT INTO Portfolios (coin, symbol, holdings, own, createdAt, updatedAt, UserEmail)
 VALUES ("bitcoin", "BTH", 2.0, true, "2018-12-06 04:05:00", "2018-12-06 04:05:00", "demo@cryptofolio.com"),
 ("ethereum", "ETH", 19.0, true, "2018-12-06 04:05:00", "2018-12-06 04:05:00", "demo@cryptofolio.com"),
-("litecoin", "LTC, 36.0, true, "2018-12-06 04:05:00", "2018-12-06 04:05:00", "demo@cryptofolio.com"),
+("litecoin", "LTC", 36.0, true, "2018-12-06 04:05:00", "2018-12-06 04:05:00", "demo@cryptofolio.com"),
 ("ripple", "XRP", 15.0, true, "2018-12-06 04:05:00", "2018-12-06 04:05:00", "demo@cryptofolio.com"),
-("monero", "XMR", 42.0, true, "2018-12-06 04:05:00", "2018-12-06 04:05:00", "demo@cryptofolio.com"),
+("monero", "XMR", 42.0, true, "2018-12-06 04:05:00", "2018-12-06 04:05:00", "demo@cryptofolio.com");
 
 INSERT INTO Transactions (type, currency, quantity, price, date, createdAt, updatedAt, UserEmail)
 VALUES ("buy", "bitcoin", 2.5, 3000.00, "2018-12-03 04:05:00", "2018-12-03 04:05:00", "2018-12-03 04:05:00", "demo@cryptofolio.com"),
