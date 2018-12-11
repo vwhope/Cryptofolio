36089aa2e48742298791bc77f045ee1e710f23eb
Kevin Luu <this.kevinluu@gmail.com>
2018-12-11 13:58:46 -0500

    added register link to login modal

--- public/js/register.js
+++ public/js/register.js
@@ -1,7 +1,31 @@
+// Hide the drop down menu
+$(".navbar .btn-group").hide();
+// Attach event listner for submit
 $("#submit").on("click", function(event) {
   event.preventDefault();
+  var $firstName = $("#input-first-name").val();
+  var $lastName = $("#input-last-name").val();
   var $email = $("#input-email").val();
   var $password = $("#input-password").val();
-  
-  console.log($email, $password);
+  var payload = {
+    firstName: $firstName,
+    lastName: $lastName,
+    email: $email,
+    password: $password
+  };
+  // AJAX call to register new user
+  $.ajax({
+    type: "POST",
+    data: payload,
+    url: "/api/register",
+    success: function(data) {
+      console.log("AJAX call successful.");
+      window.location.href = "/dashboard";
+    },
+    error: function(jqXHR, textStatus, errorThrown) {
+      // When AJAX call has failed
+      console.log("AJAX call failed.");
+      console.log(textStatus + ": " + errorThrown);
+    }
+  });
 });
